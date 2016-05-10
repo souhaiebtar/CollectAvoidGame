e25350bf6e0746542f8486334ddc9a0456df3cbe
tsouhaieb <tarhounisouhaieb@gmail.com>
2016-05-10 14:16:49 -0700

    Initial Commit

---
+++ Collect Avoid gameTests/Collect_Avoid_gameTests.swift
@@ -0,0 +1,36 @@
+//
+//  Collect_Avoid_gameTests.swift
+//  Collect Avoid gameTests
+//
+//  Created by unknown-macbook on 5/10/16.
+//  Copyright © 2016 indietarhouni.com. All rights reserved.
+//
+
+import XCTest
+@testable import Collect_Avoid_game
+
+class Collect_Avoid_gameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
