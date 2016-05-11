39324faa7c603ca3de6f24bfed73db4c5ef058eb
tsouhaieb <tarhounisouhaieb@gmail.com>
2016-05-11 10:19:18 -0700

    moving logic added

--- Collect Avoid game/GameScene.swift
+++ Collect Avoid game/GameScene.swift
@@ -13,11 +13,16 @@ var squareCollect = SKSpriteNode?()
 var circleAvoid = SKShapeNode?()
 var stars = SKSpriteNode?()
 
-struct physicsCategory {
+var squareSpeed = 0.9
+var circleSpeed = 0.9
+
+
+struct physicsCategory{
     static let player : UInt32 = 1
     static let squareCollect : UInt32 = 2
     static let circleAvoid : UInt32 = 3
 }
+
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
@@ -47,12 +52,18 @@ class GameScene: SKScene {
     
     func spawnSquareCollect(){
         squareCollect = SKSpriteNode(color: UIColor.whiteColor(), size: CGSize(width: 50, height: 50))
-        squareCollect?.position = CGPoint(x: self.frame.midX, y: 600)
+        //squareCollect?.position = CGPoint(x: self.frame.midX, y: 600)
+        squareCollect?.position = CGPoint(x: Int(arc4random_uniform(700)+300), y: 800)
+    
+        var moveForward = SKAction.moveToY(-100, duration: squareSpeed)
+        let destroy = SKAction.removeFromParent()
         squareCollect?.physicsBody = SKPhysicsBody(rectangleOfSize: squareCollect!.size)
         squareCollect?.physicsBody?.affectedByGravity = false
         squareCollect?.physicsBody?.categoryBitMask = physicsCategory.squareCollect
         squareCollect?.physicsBody?.dynamic = true
         
+        
+        squareCollect?.runAction(SKAction.sequence([moveForward, destroy]))
         self.addChild(squareCollect!)
 
         
@@ -63,15 +74,32 @@ class GameScene: SKScene {
         circleAvoid?.strokeColor = UIColor(white: 0, alpha: 0)
         circleAvoid?.fillColor = UIColor.orangeColor()
         
-        circleAvoid?.position = CGPoint(x: 400, y: 400)
+        circleAvoid?.position = CGPoint(x: Int(arc4random_uniform(700) + 300), y: 800)
+        
+        circleAvoid?.physicsBody = SKPhysicsBody(circleOfRadius: 15)
+        circleAvoid?.physicsBody?.affectedByGravity = false
+        circleAvoid?.physicsBody?.categoryBitMask = physicsCategory.circleAvoid
+        circleAvoid?.physicsBody?.dynamic = true
+        
+        
+        var moveForward = SKAction.moveToY(-100, duration: circleSpeed)
+        let destroy = SKAction.removeFromParent()
+        
+        circleAvoid?.runAction(SKAction.sequence([moveForward, destroy]))
         
         self.addChild(circleAvoid!)
     }
     
     func spawnStars(){
-        stars = SKSpriteNode(color: UIColor.whiteColor(), size: CGSize(width: 10, height: 10))
+        var randomSize = Int(arc4random_uniform(8)+1)
+        stars = SKSpriteNode(color: UIColor.whiteColor(), size: CGSize(width: randomSize, height: randomSize))
         stars?.position = CGPoint(x: self.frame.midX, y: 500)
         stars?.zPosition = -1
+        
+        var moveForward = SKAction.moveToY(-100, duration: circleSpeed)
+        let destroy = SKAction.removeFromParent()
+        
+        stars?.runAction(SKAction.sequence([moveForward, destroy]))
         
         self.addChild(stars!)
         
